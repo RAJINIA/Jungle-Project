208307d553f266b8488daa4f2c34b4ec55f8fe3b
RAJINIA <rajini1608@gmail.com>
2022-01-11 19:00:50 -0600

    Update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200731012121) do
+ActiveRecord::Schema.define(version: 20200723170211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
